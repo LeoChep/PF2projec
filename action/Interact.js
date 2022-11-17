e13f8e87f9b62a3058f45eeb8379a1c7890e68b5
LeoChep <1515038222@qq.com>
2022-11-18 02:40:39 +0800

    抽象了创造互动，便于组合式，添加期望写法

--- action/Interact.js
+++ action/Interact.js
@@ -1,4 +1,13 @@
 class Interact {
   name = "";
+
   use() {}
+
+  static createNew(name,func){
+    var interact=new Interact();
+    interact.name=name;
+    interact.use=func;
+    console.log(func)
+    return interact;
+  }
 }
--- av/test/scene1.js
+++ av/test/scene1.js
@@ -7,14 +7,23 @@ class Scene1 extends Scene {
     this.entityList = [];
     var teleport = new Entity();
     teleport.name = "传送门";
-    var forward = new Interact();
-    forward.name = "前进";
-    forward.use = function () {
+    var func = function () {
       alert("move forward");
       sceneController.loadScene(Scene2.getInstance());
       // sceneController.scene = Scene2.getInstance();
     };
+    var forward=Interact.createNew("forward",func)
     teleport.interactList = [forward];
     this.entityList = [teleport];
   }
+
+  /**
+   * 期望写法
+   * entityLiST=[A,B,C,D]
+   * mapping=[{entity:A,interaction:[a,b,c]}]
+   * 
+   * a(){
+   * }
+   * 
+   */
 }
