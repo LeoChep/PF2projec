3ff31cc55780cd1e8f3e6d599e1a485fa0d0ba6c
LeoChep <1515038222@qq.com>
2022-11-18 10:11:45 +0800

    使用config控制sceneController，删除赘余代码

--- config/config.js
+++ config/config.js
@@ -1,5 +1,10 @@
 class Config {
   static sceneController = new SceneController();
-  static getSceneController = Config.sceneController;
+  static getSceneController() {
+    return Config.sceneController;
+  }
+  static setSceneController(sceneController) {
+    Config.sceneController = sceneController;
+  }
 }
 var config = new Config();
